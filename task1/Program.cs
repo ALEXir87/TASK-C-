983be91c99c4025b6d4fea499b64e305a2380091
Aleksey <busido_na87@mail.ru>
2022-07-11 01:02:17 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Задача№2");
+Console.WriteLine("Введи число A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA < numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+    Console.Write("min = ");
+    Console.WriteLine(numberA);
+}
+if (numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+    Console.Write("min = ");
+    Console.WriteLine(numberB);
+}
