7f0b400ca9f1f8ee7f070f45dcfa877e56aa64a1
Aleksey <busido_na87@mail.ru>
2022-07-11 02:06:09 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задача№4");
+Console.WriteLine("Введи число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число C:");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+{
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
