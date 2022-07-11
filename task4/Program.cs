d782a98fada834e9d8e8b128c4262414580867f5
Aleksey <busido_na87@mail.ru>
2022-07-11 13:20:53 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Задача№8");
+Console.WriteLine("Введи число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count < N)
+{
+    Console.Write("Чётное число = ");
+    Console.WriteLine(count + 1);
+    count +=2;
+}
