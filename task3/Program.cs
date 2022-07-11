21e0d1566bcb80a6174e7f7688622b65a5f09b3f
Aleksey <busido_na87@mail.ru>
2022-07-11 12:22:07 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задача№6");
+Console.WriteLine("Введи число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N % 2 == 0)
+{
+    Console.WriteLine("Чётное число");
+}
+else
+{ 
+    Console.WriteLine("Нечётное число");
+}
